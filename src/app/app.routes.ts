9eef2df33abf731aac6838b1f2346ae3d1134e1c
UmitAkbas <umit.akbas@hotmail.com.tr>
2024-06-21 01:53:23 +0300

    fist commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
+import { TariflerComponent } from './tarifler/tarifler.component';
 
 export const routes: Routes = [
-    
+    {path:"", component:TariflerComponent}
 ];
